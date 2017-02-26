f4ad428245adee373eb3784668fc12fe7d59be7f
developerstage1 <developerstage1@gmail.com>
2017-02-26 18:14:13 +0000

    delete

--- words-ext-1.0/src/com/wr/Reader.java
+++
@@ -1,8 +0,0 @@
-package com.wr;
-
-import java.io.FileNotFoundException;
-import java.util.List;
-
-public interface Reader {
-	public List<String> read(String path) throws FileNotFoundException;
-}
